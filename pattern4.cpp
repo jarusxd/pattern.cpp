cb748aff26779737d37f6cf2d7da88f46125776c
jarusxd <116182297+jarusxd@users.noreply.github.com>
2023-03-01 21:52:01 +0530

    Create pattern4.cpp

---
+++ pattern4.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int star=1,i,j,k;
+	int n;
+	cout<<"Enter a num: ";
+	cin>>n;
+	int space =n-1;
+	for(i=0;i<n;i++){
+		for(j=0;j<space;j++){
+			cout<<" ";
+		}
+		for(k=0;k<star;k++)
+		{
+		 cout<<"*"<<" ";
+		}
+		cout<<endl;
+		star++;
+		space--;
+	}
+	int space1=0;
+	int star1=n;
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<space1;j++)
+		{
+			cout<<" ";
+		}
+		for(k=0;k<star1;k++){
+			cout<<"*"<<" ";
+		}
+		space1++;
+		star1--;
+		cout<<endl;
+	}
+}
